ca2e5a0e52fbd7f263af7997431593ffdd75d35a
wildanrizkii <wildanrizki9560@gmail.com>
2023-09-26 19:16:48 +0700

    kasir

--- src/pages/Kasir/index.js
+++ src/pages/Kasir/index.js
@@ -1,7 +1,57 @@
 import Layout from "../../../components/Layout";
 import Head from "next/head";
-export default function Kasir() {
-    let dataObat;
+import handlerQuery from "../../../lib/db";
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+export default function Kasir({
+  hasil
+}) {
+  let item = [];
+  const onClickTambah = (e) => {
+    
+    item.push(e.target.value)
+  } 
+  console.log(item[0])
+
+  let semuaData;
+  const router = useRouter();
+  let index = 0;
+  try {
+    semuaData = hasil.map((x) => {
+      index = index + 1;
+      return (
+        <tr key={x.id_item} style={{ fontWeight: "bold" }}>
+          <td className="is-vcentered">{index}</td>
+          <td className="is-vcentered">{x.id_item}</td>
+          <td className="is-vcentered">{x.nama_item}</td>
+          <td className="is-vcentered">{x.stok}</td>
+          <td className="is-vcentered">{x.nama_satuan}</td>
+          <td className="is-vcentered">{x.nama_jenis}</td>
+          <td className="is-vcentered">{x.harga}</td>
+          <td className="is-vcentered">{x.nama_rak}</td>
+          <td className="is-vcentered">-</td>
+          <td className="is-vcentered">
+            <button
+              className="button is-success"
+              onClick={() => onClickTambah(x.id_item)}
+            >
+              +
+            </button>
+          </td>
+        </tr>
+      );
+      console.log(semuaData);
+    });
+  } catch (e) {
+    semuaData = (
+      <tr>
+        <td colSpan="7" className="is-vcentered">
+          {hasil}
+        </td>
+      </tr>
+    );
+  }
 
   return (
     <>
@@ -25,21 +75,39 @@ export default function Kasir() {
             <th className="has-text-centered is-vcentered">Tambah</th>
           </tr>
         </thead>
-        <tbody>{}</tbody>
+        <tbody>{semuaData}</tbody>
       </table>
 
-      
+
     </>
   );
 }
 
-// export async function getServerSideProps(context){
-//   let dataObat = 
-//   "SELECT id_item, nama, stok, satuan.nama, jenis.nama, history_harga_jual.harga, rak.nama_rak " +
-//   "FROM item INNER JOIN satuan on item.id_satuan = satuan.id_satuan " +
-//     "INNER JOIN jenis on item.id_jenis = jenis.id_jenis " +
-//     "INNJER JOIN rak on item.id_rak = rak.id_rak"
-// }
+export async function getServerSideProps(context) {
+  let query =
+  "SELECT item.id_item, item.nama as nama_item, stok, satuan.nama as nama_satuan, jenis.nama as nama_jenis, history_harga_jual.harga, rak.nama_rak " +
+  "FROM item INNER JOIN satuan on item.id_satuan = satuan.id_satuan INNER JOIN jenis on item.id_jenis_item = jenis.id_jenis " + 
+  "INNER JOIN rak on item.id_rak = rak.id_rak INNER JOIN history_harga_jual on item.id_item = history_harga_jual.id_item "
+
+  try {
+    const getData = await handlerQuery({ query });
+    const hasil = JSON.parse(JSON.stringify(getData));
+    // const getUser = await handlerQuery({ query: queryUser});
+    // const user = JSON.parse(JSON.stringify(getUser));
+    // user.unshift({ idUser: "", username: "SEMUA" });
+    return {
+      props: {
+        hasil
+      },
+    };
+  } catch (e) {
+    return {
+      props: {
+        hasil: e.message
+      },
+    };
+  }
+}
 
 Kasir.getLayout = function getLayout(page) {
   return <Layout clicked="Kasir">{page}</Layout>;
